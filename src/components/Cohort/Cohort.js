cc4395ad88a30de1c556e9146996ad8100321f45
stefnotadevyet <srjack91@gmail.com>
2021-06-21 06:37:59 -0400

    wrote basic cohort and person function components

--- src/components/Cohort/Cohort.js
+++ src/components/Cohort/Cohort.js
@@ -3,5 +3,12 @@ import Person from '../Person/Person';
 import './Cohort.css';
 
 // COHORT COMPONENT CODE GOES HERE
+function Cohort(props) {
+  return (
+    <div>
+      <Person />
+    </div>
+  )
+};
 
 export default Cohort;
--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -2,5 +2,10 @@ import React from 'react';
 import './Person.css';
 
 // PERSON COMPONENT CODE GOES HERE
+function Person(props) {
+  return (
+    <div>test</div>
+  )
+};
 
 export default Person;
